a97a3976f982d669c1eb6d655c1974eba7e00469
BAe <bae0712bar@gmail.com>
2021-04-18 12:25:13 +0800

    count

--- r.py
+++ r.py
@@ -1,7 +1,9 @@
 import random
 
 r = random.randint(1, 100)
+count = 0 
 while True:
+	count += 1
 	num = input('>>請猜一個數字')
 	num = int(num)
 	if num > r:
@@ -10,4 +12,6 @@ while True:
 		print('再猜一次 數字大點')
 	else:
 		print('BINGO')
+		print('這是你的第', count, '次')
 		break
+	print('這是你的第', count, '次')
